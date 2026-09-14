f317d0bebab4bda6aae2ce1cae22f6819dabf7ee
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-09-14 13:29:32 +0300

    error handling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import morgan from 'morgan';
 import userRouter from './routes/userRoutes.js';
 import tourRouter from './routes/tourRoutes.js';
+import AppError from './utils/appError.js';
+import globalErrorController from './errorController.js';
 
 const app = express();
 if (process.env.NODE_ENV === 'development') {
@@ -13,4 +15,12 @@ app.use(express.static(`public`));
 // mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  // const err = new Error();
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError('fail', 404));
+});
+app.use(globalErrorController);
 export default app;
---
+++ controllers/errorController.js
@@ -0,0 +1,5 @@
+export default (err, req, res, next) => {
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+};
