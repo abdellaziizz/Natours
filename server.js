5482258533610261473639ee15c3dbe69f8b2640
ahmed abdelaziz <a.abdelaziz2338@gmail.com>
2026-09-15 00:08:46 +0300

    handling connection promise

--- server.js
+++ server.js
@@ -8,10 +8,26 @@ export const getOverview = (req, res) => {
     mapboxToken: process.env.MAPBOX_TOKEN,
   });
 };
-const DB = process.env.DATABASE;
-mongoose.connect(DB).then((con) => {
-  console.log('DB connection Success');
-});
-app.listen(3000, '127.0.0.1', () => {
+const server = app.listen(3000, '127.0.0.1', () => {
   console.log('app is running on port 3000');
+});
+
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log('DB connection Success');
+  })
+  .catch((err) => {});
+process.on('unhandledRejection', () => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+process.on('uncaughtException', () => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
